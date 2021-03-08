56c52a7250ecda706cef37f565cf9e18ef263f2b
Daniel <danielkarpen97@gmail.com>
2021-03-08 17:48:16 -0600

    update movies state from tmdb

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+const Card = ({ title, path, date, rating }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <figure>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${path}`}
+          alt={`Movie poster for ${title}`}
+        />
+        <figcaption>
+          {date} - {rating}
+        </figcaption>
+      </figure>
+      <button>More Info!</button>
+    </section>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+};
+
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import api from "api";
 import { Main } from "components";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return <Main>Hello World</Main>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await api.index();
+      setMovies(() => results);
+    })();
+  }, []);
+
+  console.log(movies);
+
+  return <Main></Main>;
 };
 
 export default Home;
